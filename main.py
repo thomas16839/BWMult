65b17477ba2c37d31064dff35eec89bc93c57e9e
Thomas Pouwels <thomas16839@gmail.com>
2023-06-07 12:05:22 +0200

    file name and header now include width of the adder for clarity

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ import adders
 import sys
 
 if __name__ == '__main__':
-    f = open('multiplier.vhd', 'w')
-
     try:
         width = int(sys.argv[1])
     except:
         print('Please specify (integer) width, defaulting to 32')
         width = 32
 
+    f = open(f'multiplier_{width}.vhd', 'w')
     # Write the header part (fixed) and the partial product signals (trivial)
-    header.write_header(f)
+    header.write_header(f, width)
     partial_products.print_signals(f, width)
 
     # initialise the grid and solve it
