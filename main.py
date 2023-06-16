59fe7ea0503a7a149cf850f62cf376f5e84db7a8
Thomas Pouwels <tpouwels@student.tudelft.nl>
2023-06-16 15:32:52 +0200

    made a mistake in the merge, fixed now

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ import adders
 import sys
 
 if __name__ == '__main__':
-    f = open('baughwooley.vhd', 'w')
-
     try:
         width = int(sys.argv[1])
     except:
         print('Please specify (integer) width, defaulting to 32')
         width = 32
 
-    f = open(f'multiplier_{width}.vhd', 'w')
+    f = open(f'baughwooley_{width}.vhd', 'w')
     # Write the header part (fixed) and the partial product signals (trivial)
     header.write_header(f, width)
     partial_products.print_signals(f, width)
